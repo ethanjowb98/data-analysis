46a405330316553bf9f6dfdd2e56ca71ecd9859a
Ethan Bascug <efbascug@bposeats.com>
2024-05-12 05:02:21 +0800

    [act4] Used all dataset to train the model

---
+++ activity_4/activity_4.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import seaborn as sb
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plot
+from sklearn import metrics
+
+headers = [
+    "os_type",
+    "data_bytes",
+    "time"
+]
+
+dataset = pd.read_csv("act4.csv", header=None, names=headers)
+dataset.drop(index=dataset.index[0], axis="index", inplace=True)
+dataset["os_type"] = dataset["os_type"].astype("category").cat.codes
+
+feature_var = dataset.drop(columns="time")  # X
+dependent_var = dataset["time"]  # Y
+
+feat_var_train, feat_var_test, dep_var_train, dep_var_test = train_test_split(
+    feature_var,
+    dependent_var,
+    test_size=0.30
+)
+
+model = LinearRegression()
+model.fit(feature_var, dependent_var)
+
+dep_var_predict = model.predict(feature_var)
+
+plot.scatter(dependent_var, dep_var_predict)
+plot.xlabel("Actual Time")
+plot.ylabel("Predicted Time")
+
+score = metrics.r2_score(dependent_var, dep_var_predict)
+
+with open("summary.txt", "w") as file:
+    file.write(str(float(score)))
+
+plot.savefig("scatterplot.png")
